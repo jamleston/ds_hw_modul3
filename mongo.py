e20462d3a563f6d3f6d5e1c9e1a799c13e5e4a1c
jamleston <jamlessston@gmail.com>
2024-09-25 14:56:25 +0200

    cats database

--- mongo.py
+++ mongo.py
@@ -37,3 +37,64 @@ result_many = db.cats.insert_many(
         },
     ]
 )
+
+# 
+# read
+# 
+
+def showAll():
+    obj = db.cats.find({})
+    result = []
+    for el in obj:
+        result.append(el)
+    return result
+
+# res = showAll()
+# print(res)
+
+def showCatById(id):
+    result = db.cats.find_one({"_id": ObjectId(id)})
+    return result
+
+# res = showCatById('66f4022d81b181920b27d926')
+# print(res)
+
+# 
+# update
+# 
+
+def updateAge(name, age):
+    db.cats.update_one({"name": name}, {"$set": {"age": age}})
+    result = db.cats.find_one({"name": name})
+    return result
+
+# res = updateAge('barsik', 10000000)
+# print(res)
+
+def addNewFeature(name, feature):
+    db.cats.update_one({'name': name}, {'$addToSet': {'features': feature}})
+    result = db.cats.find_one({"name": name})
+    return result
+
+# res = addNewFeature('barsik', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+# print(res)
+
+# 
+# delete
+# 
+
+def deleteByName(name):
+    db.cats.delete_one({"name": name})
+    result = db.cats.find_one({"name": name})
+    return result
+
+# res = deleteByName('barsik')
+# print(res)
+
+def deleteAll():
+    db.cats.deleteMany({})
+    result = db.cats.find({})
+    return result
+
+# res = deleteAll()
+# print(res)
